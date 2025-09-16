d4d8897f1c5fff172006a1bf31848f098ba89708
aditya01prog <adityaa0933@gmail.com>
2025-09-16 23:00:38 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import requests
 query = input("On what topic would you like to search a news ? ") 
-api = "e73588e1d63b4df798a9824d2d47eeba"
+api = "your_api_key"
 
 url = f"https://newsapi.org/v2/everything?q={query}&from=2025-08-06&to=2025-08-06&sortBy=popularity&apiKey={api}"
 
@@ -13,4 +13,5 @@ articles = data["articles"]
 for index, article in enumerate(articles):
     print(index+1, article["title"])
     print(article["url"])
+
     print("\n*************************************\n")
